b62615f143c679f83c40009f59e030555fbb3304
Martin Pavlov <marto.pawlow@gmail.com>
2024-12-07 08:16:08 +0200

    Move comment

--- 2024/7.js
+++ 2024/7.js
@@ -8,18 +8,18 @@ function day7(input, isPartTwo) {
     const digits = operatorLineSections[1].split(" ");
     let possibleResultsArray = [];
 
+    /**
+     * Recreate a new array for each digit to reflect the new possible
+     * result. For example, if we have [1, 2, 3] as digits:
+     * - after first step => [1]
+     * - after second step => [3, 2, 12]
+     * - after third step => [6, 9, 33, 5, 6, 23, 15, 36, 123]
+     */
     for (let i = 0; i < digits.length; i++) {
       const numberDigit = Number(digits[i]);
       if (i === 0) {
         possibleResultsArray.push(numberDigit);
       } else {
-        /**
-         * Recreate a new array for each digit to reflect the new possible
-         * result. For example if we have [1, 2, 3] as digits:
-         * - after first step => [1]
-         * - after second step => [3, 2, 12]
-         * - after third step => [6, 9, 33, 5, 6, 23, 15, 36, 123]
-         */
         possibleResultsArray = possibleResultsArray.reduce(
           (newArray, total) => {
             newArray.push(total + numberDigit);
