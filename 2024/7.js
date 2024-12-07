e000d8d4a88af51c27f838b0a4a06cc7b9ca00e3
Martin Pavlov <marto.pawlow@gmail.com>
2024-12-07 08:13:42 +0200

    Add solution for seventh day

---
+++ 2024/7.js
@@ -0,0 +1,46 @@
+function day7(input, isPartTwo) {
+  const operatorLines = input.split("\r\n");
+  let sum = 0;
+
+  operatorLines.forEach((operatorLine) => {
+    const operatorLineSections = operatorLine.split(": ");
+    const result = Number(operatorLineSections[0]);
+    const digits = operatorLineSections[1].split(" ");
+    let possibleResultsArray = [];
+
+    for (let i = 0; i < digits.length; i++) {
+      const numberDigit = Number(digits[i]);
+      if (i === 0) {
+        possibleResultsArray.push(numberDigit);
+      } else {
+        /**
+         * Recreate a new array for each digit to reflect the new possible
+         * result. For example if we have [1, 2, 3] as digits:
+         * - after first step => [1]
+         * - after second step => [3, 2, 12]
+         * - after third step => [6, 9, 33, 5, 6, 23, 15, 36, 123]
+         */
+        possibleResultsArray = possibleResultsArray.reduce(
+          (newArray, total) => {
+            newArray.push(total + numberDigit);
+            newArray.push(total * numberDigit);
+            if (isPartTwo) {
+              newArray.push(
+                Number(total.toString().concat(numberDigit.toString()))
+              );
+            }
+
+            return newArray;
+          },
+          []
+        );
+      }
+    }
+
+    sum += !!possibleResultsArray.find((item) => item === result) ? result : 0;
+  });
+
+  return sum;
+}
+
+module.exports = { day7 };
--- 2024/index.js
+++ 2024/index.js
@@ -4,5 +4,6 @@ const { day3 } = require("./3");
 const { day4 } = require("./4");
 const { day5 } = require("./5");
 const { day6 } = require("./6");
+const { day7 } = require("./7");
 
-module.exports = { day1, day2, day3, day4, day5, day6 };
+module.exports = { day1, day2, day3, day4, day5, day6, day7 };
